e021096eb5aa04cca2ef03e69a0d0448a151595f
Patrice PETIT <patricepetit@hotmail.com>
2018-09-19 21:44:32 +0200

    Docker: pm2 watch mode is working

--- Dockerfile
+++ Dockerfile
@@ -1,18 +1,12 @@
-FROM node:latest
+FROM keymetrics/pm2:latest
 
 WORKDIR /app
 
 COPY ./src /app
-COPY ./ecosystem.config.js /app/ecosystem.config.js
 COPY ./package.json /app/package.json
-
-RUN npm install -g pm2
 
 RUN npm install --only=production && \
 npm cache clean --force
-
-RUN apt-get install -y \
-curl
 
 CMD /bin/bash
 
--- ecosystem.config.js
+++ ecosystem.config.js
@@ -1,12 +1,21 @@
+var path = require("path")
+var processName = path.basename(process.argv[1]);
+var indexPath = "./server/index.js"
+
+if (processName === "pm2-dev") {
+  indexPath = path.join("src", indexPath)
+}
+
 module.exports = {
   apps : [
 
     // First application
     {
       name      : 'CENTRAL',
-      script    : 'lib/index.js',
+      script    : indexPath,
       "error_file": "/dev/stderr",
       "out_file": "/dev/stdout",
+      "ignore-watch" : [".git", "ecosystem.config.js"]
     }
   ]
 };
