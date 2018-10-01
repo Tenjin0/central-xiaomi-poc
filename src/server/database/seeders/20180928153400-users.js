6535d55a55340a0fea344f61b2f85d6dd14e8727
Patrice <ppetit@wynd.eu>
2018-10-01 10:41:15 +0200

    Core: add user seeder

---
+++ src/server/database/seeders/20180928153400-users.js
@@ -0,0 +1,31 @@
+'use strict';
+
+module.exports = {
+  up: (queryInterface, Sequelize) => {
+    /*
+      Add altering commands here.
+      Return a promise to correctly handle asynchronicity.
+
+      Example:
+      return queryInterface.bulkInsert('Person', [{
+        name: 'John Doe',
+        isBetaMember: false
+      }], {});
+    */
+    return queryInterface.bulkInsert('Users', [{
+      first_name: "Patrice",
+      last_name: "PETIT",
+      card_content: "275ccc0b"
+    }], {});
+  },
+
+  down: (queryInterface, Sequelize) => {
+    /*
+      Add reverting commands here.
+      Return a promise to correctly handle asynchronicity.
+
+      Example:
+      return queryInterface.bulkDelete('Person', null, {});
+    */
+  }
+};
