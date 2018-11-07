b2a0d6f77e23e14094badab15a0d8d8e2ac76441
Patrice <ppetit@wynd.eu>
2018-11-07 18:00:10 +0100

    Api: try add more users sample

---
+++ api/database/seeders/20181107164832-more-users.js
@@ -0,0 +1,39 @@
+function generateUsers(number) {
+
+	const users = [];
+	for (let i = 0; i < number; i++) {
+
+	}
+}
+
+function generateUser(number) {
+
+}
+module.exports = {
+
+	up: queryInterface => queryInterface.bulkInsert('Users', [{
+		first_name: 'prenom1',
+		last_name: 'nom1',
+		card_data: 'aaaaaaaa',
+		created_at: new Date(),
+	},
+	{
+		first_name: 'prenom2',
+		last_name: 'nom2',
+		card_data: 'aaaaaaab',
+		created_at: new Date(),
+	},
+
+	], {}),
+
+
+	down: (queryInterface, Sequelize) => {
+		/*
+		  Add reverting commands here.
+		  Return a promise to correctly handle asynchronicity.
+
+		  Example:
+		  return queryInterface.bulkDelete('People', null, {});
+		*/
+	}
+};
